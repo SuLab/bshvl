1df500feecbf6fc60d575df0fc01df1791215440
Emily Mallory <edoughty@stanford.edu>
2015-10-14 17:05:16 -0700

    reverted gene_relations to earlier version and updated load_docids for standard datasets from deepdive

--- py/load_docids.py
+++ py/load_docids.py
@@ -11,11 +11,8 @@
 
 from helper.easierlife import *
 
-#location of the text corpus. Add after data
-CORPUS = "/data/"
-
-INPUT_FOLDER = BASE_FOLDER + "/data/RS_MatteoPlos.37.tocp"
-INPUT_FOLDER2 = BASE_FOLDER + "/data/RS_MatteoPlos.37.tocp_batch2"
+#location of the text corpus.
+INPUT_FOLDER = BASE_FOLDER + "/data/plos_full_conll"
 INPUT_NEG_FOLDER = BASE_FOLDER + "/data/NEGARTICLE"
 INPUT_FILE_GS_SKIP = BASE_FOLDER + "/data/plos_journals_dip_mint_pmids.txt"
 INPUT_FILE_10K_SKIP = BASE_FOLDER + "/data/plos_docids_sample_10000.txt"
@@ -33,19 +30,9 @@ log("~~~~~~~~~~~~~~~~~" + INPUT_FOLDER)
 
 for docid in os.listdir(INPUT_FOLDER):
 	if docid.startswith('journal.pbio.') or docid.startswith('journal.pgen.') or docid.startswith('journal.pone.'): 
-		if "input.text" in os.listdir(INPUT_FOLDER + "/" + docid):
-			folder = INPUT_FOLDER + "/" + docid + "/input.text"
-			if docid.split(".pdf")[0] not in pmids_skip and docid not in pmids_skip:
-				print json.dumps({"docid": docid, "folder": folder})
-
-log("~~~~~~~~~~~~~~~~~" + INPUT_FOLDER2)
-
-for docid in os.listdir(INPUT_FOLDER2):
-	if docid.startswith('journal.pbio.') or docid.startswith('journal.pgen.') or docid.startswith('journal.pone.'): 
-		if "input.text" in os.listdir(INPUT_FOLDER2 + "/" + docid):
-			folder = INPUT_FOLDER2 + "/" + docid + "/input.text"
-			if docid.split(".pdf")[0] not in pmids_skip and docid not in pmids_skip:
-				print json.dumps({"docid": docid, "folder": folder})
+		folder = INPUT_FOLDER + "/" + docid 
+		if docid.split(".pdf")[0] not in pmids_skip and docid.split(".nlp")[0] not in pmids_skip:
+			print json.dumps({"docid": docid.split(".nlp")[0], "folder": folder})
 
 
 log("~~~~~~~~~~~~~~~~~" + INPUT_NEG_FOLDER)
