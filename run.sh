15623320981e7006b952da6fcf0f1eef44997706
Sandip Chatterjee <sandip.chatterjee@gmail.com>
2016-02-06 11:28:07 -0800

    updates initialization bash script for deepdive 0.7.1

--- run.sh
+++ run.sh
@@ -1,22 +1,32 @@
 #! /usr/bin/env bash
 
-export APP_HOME=`pwd`
-export DEEPDIVE_HOME=`cd ${APP_HOME}/../../../deepdive; pwd`
+export APP_HOME=/home/sandip/deepdive-0.7.1/app/deepdive_genegene_app
+#export DEEPDIVE_HOME=`cd ${APP_HOME}/../../../deepdive; pwd`
 
 # Database Configuration
-export PGUSER=${PGUSER:-`whoami`}
-export PGPASSWORD=${PGPASSWORD:-}
-export PGDATABASE=${PGDATABASE}
-export PGHOST=${PGHOST}
-export PGPORT=${PGPORT}
-export PARALLELISM=48
-export MEMORY="32g"
+export PGUSER="sandip"
+export PGPASSWORD=""
+export PGDATABASE="deepdive_genegene"
+export PGHOST="localhost"
+export PGPORT=5432
+
+#export PGUSER=${PGUSER:-`whoami`}
+#export PGPASSWORD=${PGPASSWORD:-}
+#export PGDATABASE=${PGDATABASE}
+#export PGHOST=${PGHOST}
+#export PGPORT=${PGPORT}
+export PARALLELISM="4"
+export MEMORY="64g"
 export JAVA_OPTS="-Xmx"$MEMORY
 export SBT_OPTS="-Xmx"$MEMORY
 
 export LD_LIBRARY_PATH=[DEEPDIVE_HOME]/util/dw_linux/lib/protobuf/lib:[DEEPDIVE_HOME]/util/dw_linux/lib64
-export PYTHONPATH=$DEEPDIVE_HOME/ddlib/:$PYTHONPATH
+#export PYTHONPATH=$DEEPDIVE_HOME/ddlib/:$PYTHONPATH
 
-cd $DEEPDIVE_HOME
+#cd $DEEPDIVE_HOME
+cd $APP_HOME
 
-$DEEPDIVE_HOME/sbt/sbt "run -c $APP_HOME/application.conf" $@
+deepdive initdb
+deepdive run
+
+#$DEEPDIVE_HOME/sbt/sbt "run -c $APP_HOME/application.conf" $@
