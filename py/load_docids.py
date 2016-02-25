67f1fac8b04aea151b428bc556607079e311d487
Tong Shu Li <tongli@scripps.edu>
2016-02-25 15:39:47 -0800

    Simplify load_docids().
    The old version did nothing with the set of good training data files, so I deleted
    that part of the code. Older versions may have used that to properly skip over
    some files, but I have no idea if this is true.

--- py/load_docids.py
+++ py/load_docids.py
@@ -1,43 +1,36 @@
-#! /usr/bin/env python
-
+#!/usr/bin/env python
 """
 Author: Emily K Mallory and Ce Zhang
 Date: 08/24/15
 Contact: emily.mallory@stanford.edu
 
-Loads docids into database. 
+Loads docids into database.
+
+Refactored by: Tong Shu Li
+Last updated: 2016-02-25
 """
+import json
+import os
 
-from helper.easierlife import *
+from helper.easierlife import log
+from helper.easierlife import BASE_FOLDER
 
-#location of the text corpus.
+# location of the text corpus.
 INPUT_FOLDER = BASE_FOLDER + "/data/plos_full_conll"
 INPUT_NEG_FOLDER = BASE_FOLDER + "/data/NEGARTICLE"
-INPUT_FILE_GS_SKIP = BASE_FOLDER + "/data/plos_journals_dip_mint_pmids.txt"
-INPUT_FILE_10K_SKIP = BASE_FOLDER + "/data/plos_docids_sample_10000.txt"
-
-pmids_skip = set()
-for x in [x.strip().split("\t")[0] for x in open(INPUT_FILE_GS_SKIP).readlines()]:
-	pass
-	#pmids_skip.add(x.rstrip().lower())
-
-for x in [x.strip() for x in open(INPUT_FILE_10K_SKIP).readlines()]:
-    pass
-	#pmids_skip.add(x.rstrip().lower())
 
 log("~~~~~~~~~~~~~~~~~" + INPUT_FOLDER)
-
 for docid in os.listdir(INPUT_FOLDER):
-	if docid.startswith('journal.pbio.') or docid.startswith('journal.pgen.') or docid.startswith('journal.pone.'): 
-		folder = INPUT_FOLDER + "/" + docid 
-		if docid.split(".pdf")[0] not in pmids_skip and docid.split(".nlp")[0] not in pmids_skip:
-			print json.dumps({"docid": docid.split(".nlp")[0], "folder": folder})
+    if (docid.startswith("journal.pbio.")
+        or docid.startswith("journal.pgen.")
+        or docid.startswith("journal.pone.")):
 
+        file_name = os.path.join(INPUT_FOLDER, docid)
+
+        print json.dumps({"docid": docid.rstrip(".nlp"), "folder": file_name})
 
 log("~~~~~~~~~~~~~~~~~" + INPUT_NEG_FOLDER)
-
 for docid in os.listdir(INPUT_NEG_FOLDER):
-	if docid.startswith('.'): continue
-	folder = INPUT_NEG_FOLDER + "/" + docid
-	if docid.split(".")[0] not in pmids_skip and docid not in pmids_skip:
-		print json.dumps({"docid": docid, "folder": folder})
+    if not docid.startswith("."):
+        file_name = os.path.join(INPUT_NEG_FOLDER, docid)
+        print json.dumps({"docid": docid, "folder": file_name})
