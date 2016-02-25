f94df5acd5f132b2afc5ec168f3181c246a4077b
Tong Shu Li <tongli@scripps.edu>
2016-02-25 15:09:51 -0800

    Reformat and simplify easierlife functions.

--- py/helper/easierlife.py
+++ py/helper/easierlife.py
@@ -5,6 +5,9 @@ Date: 08/24/15
 Contact: emily.mallory@stanford.edu
 
 Helper functions for application.
+
+Refactored by: Tong Shu Li
+Last updated: 2016-02-25
 """
 import fileinput
 import json
@@ -20,14 +23,13 @@ import os
 BASE_FOLDER, throwaway = os.path.split(os.path.realpath(__file__))
 BASE_FOLDER = BASE_FOLDER + "/../../"
 
-
 def get_all_phrases_in_sentence(sent, max_phrase_length):
-	for start in range(0, len(sent.words)):
-		for end in reversed(range(start + 1, min(len(sent.words), start + 1 + max_phrase_length))):
-			yield (start, end)
+    for start in range(0, len(sent.words)):
+        for end in reversed(range(start + 1, min(len(sent.words), start + 1 + max_phrase_length))):
+            yield (start, end)
 
-def log(str):
-	sys.stderr.write(str.__repr__() + "\n")
+def log(s):
+    sys.stderr.write(s.__repr__() + "\n")
 
 def asciiCompress(data, level=9):
     """ compress data to printable ascii-code """
@@ -46,12 +48,10 @@ def asciiDecompress(code):
     return data
 
 def serialize(obj):
-	#return zlib.compress(pickle.dumps(obj))
-	return asciiCompress(pickle.dumps(obj))
+    return asciiCompress(pickle.dumps(obj))
 
 def deserialize(obj):
-	#return pickle.loads(str(unicode(obj)))
-	return pickle.loads(asciiDecompress(obj.encode("utf-8")))
+    return pickle.loads(asciiDecompress(obj.encode("utf-8")))
 
 def get_inputs():
     for line in fileinput.input():
