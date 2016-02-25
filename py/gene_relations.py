04d544db4c875c9d981ab6e15140bfc5f9d27ec0
Tong Shu Li <tongli@scripps.edu>
2016-02-24 17:18:54 -0800

    Reintroduce off-by-one bug in dep_path().
    Reintroduce off-by-one error in get_common_root() of dep_path() in gene_relations.py
    
    The original common root finding function has an off-by-one error which is caused
    by the function breaking too early. The out-of-bounds prevention statement causes
    the code to terminate one element too early, thereby effectively never checking
    the first element of the second list (path2) when the first list (path1) is longer.
    
    Since the correct version of the function does not match the original buggy code,
    this bug has been intentionally reintroduced in order to match the original code's
    behaviour.
    
    To remove the bug, simply change len(pathB) - 1 to len(pathB) in the function.

--- py/gene_relations.py
+++ py/gene_relations.py
@@ -76,8 +76,48 @@ def dep_path(deptree, sent, lemma, start1, start2):
         return path
 
     def get_common_root(pathA, pathB):
+        """This function __should__ compare two lists of potentially unequal length
+        and look for the longest suffix list that is the same in the two lists.
+        It should return the first element in that suffix list.
+
+        Example: if given [1, 2, 3, 4, 4, 5] and [1, 1, 1, 3, 4, 5], the function
+        should return the element 4, because [4, 5] is the longest suffix list.
+
+        WARNING:
+
+        However, in the original code for this function, Emily Mallory made an
+        off-by-one error. The original code does not check the first element of
+        the second list, and therefore the results are one smaller than what they
+        should be.
+
+        The original code was:
+
+        commonroot = None
+        for i in range(0, len(path1)):
+            j = len(path1) - 1 - i
+            if -i-1 <= -len(path2) or path1[j]["current"] != path2[-i-1]["current"]:
+                break
+
+            commonroot = path1[j]["current"]
+
+        The first part of the if statement is used to prevent accessing path2
+        out of bounds when the list path1 is longer than path2. However, when
+        -i-1 == len(path2), this is still within bounds of path2. As in,
+        list[0] == list[-len(list)]. However, the original code breaks at this
+        point instead of checking the last element due to the <= operator. The
+        correct operator should use < and not <=.
+
+        Since the refactored code is trying to match the original code exactly,
+        this off-by-one bug has been intentionally left the same.
+
+        To remove this off-by-one error, simply change "len(pathB) - 1" to
+        "len(pathB)" in the while loop below.
+
+        Tong Shu Li
+        Last updated: 2016-02-24
+        """
         pos = 1
-        while (pos <= len(pathA) and pos <= len(pathB)
+        while (pos <= len(pathA) and pos <= len(pathB) - 1
             and pathA[-pos]["current"] == pathB[-pos]["current"]):
             pos += 1
 
@@ -113,7 +153,6 @@ def dep_path(deptree, sent, lemma, start1, start2):
                 if i == 0:
                     w = ""
                 right_path = (w + "<-" + path2[i]["label"] + "--" ) + right_path
-
 
 
         if commonroot == start1 or commonroot == start2:
