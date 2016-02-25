9ae324f73095b7351f13a9208154483379200fe9
Tong Shu Li <tongli@scripps.edu>
2016-02-25 15:55:40 -0800

    Simplify load_sents()
    De-obfuscated the import statements of load_sents().

--- py/load_sents.py
+++ py/load_sents.py
@@ -1,26 +1,30 @@
-#! /usr/bin/env python
-
+#!/usr/bin/env python
 """
 Author: Emily K Mallory and Ce Zhang
 Date: 08/24/15
 Contact: emily.mallory@stanford.edu
 
-Loads sentences into database. 
+Loads sentences into database.
 """
+import json
 
-from helper.easierlife import *
 #from extractor.EntityExtractor_Drug import *
 
+from helper.easierlife import log
+from helper.easierlife import serialize
+from helper.easierlife import deserialize
+from helper.easierlife import get_inputs
+
 for row in get_inputs():
-	doc = deserialize(row["document"])
-	log(doc.docid)
+    doc = deserialize(row["document"])
+    log(doc.docid)
 
-	for sent in doc.sents:
-		sentence_text = " ".join([x.word for x in sent.words])
-		print json.dumps({
-			"docid": doc.docid, 
-			"sentid":sent.sentid, 
-			"sentence": serialize(sent),
-			"text": sentence_text
-    })
+    for sent in doc.sents:
+        sentence_text = " ".join(word.word for word in sent.words)
 
+        print json.dumps({
+            "docid": doc.docid,
+            "sentid": sent.sentid,
+            "sentence": serialize(sent),
+            "text": sentence_text
+        })
