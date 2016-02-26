7f56282f6dcdf7e487a3209e6c21808c4c19bc69
Sandip Chatterjee <sandip.chatterjee@gmail.com>
2016-02-25 17:52:36 -0800

    Checks and fixes util symlink before run

--- run.sh
+++ run.sh
@@ -3,6 +3,14 @@
 export APP_HOME=/home/sandip/deepdive-0.7.1/app/bshvl
 export PATH=~/local/bin:$PATH
 
+cd $APP_HOME
+
+# check if util symlink is valid (set to ~/local/util if not)
+if [ ! -e "util" ] ; then
+    echo "util directory missing; symlinking ~/local/util";
+    rm util && ln -s ~/local/util .;
+fi
+
 echo "Running DeepDive located at `which deepdive`"
 echo "Running DD application at $APP_HOME"
 
@@ -20,8 +28,6 @@ export SBT_OPTS="-Xmx"$MEMORY
 
 export LD_LIBRARY_PATH=[DEEPDIVE_HOME]/util/dw_linux/lib/protobuf/lib:[DEEPDIVE_HOME]/util/dw_linux/lib64
 #export PYTHONPATH=$DEEPDIVE_HOME/ddlib/:$PYTHONPATH
-
-cd $APP_HOME
 
 deepdive initdb
 deepdive run
