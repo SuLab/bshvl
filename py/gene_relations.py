9873837da6b39789fe46717c62ca419c35d905a9
Tong Shu Li <tongli@scripps.edu>
2016-02-25 17:44:57 -0800

    Move text normalization functions to separate file.

--- py/gene_relations.py
+++ py/gene_relations.py
@@ -33,6 +33,11 @@ from dependency_tree import dep_path
 
 from load_dicts import get_gold_std_docids
 
+from normalize_text import normalize
+from normalize_text import normalize_utf
+
+#-------------------------------------------------------------------------------
+
 #extractor dictionaries
 dict_gene_symbols_all = {}
 dict_gene_pruned = {}
@@ -290,64 +295,6 @@ def load_dict():
         for row in reader:
             dict_domains[row[0].rstrip()] = 1
 
-
-def normalize(word):
-    """
-    Name: normalize
-    Input: word
-    Return: normalized word
-
-    Normalization for word characters
-    """
-    return word.encode("ascii", "ignore").replace("'", '_').replace('{', '-_-').replace('}','-__-').replace('"', '-___-').replace(', ,', ',__')
-
-
-def normalize_utf(word):
-    """
-    Name: normalize_utf
-    Input: text word
-    Return: normalized word
-
-    Replaces common UTF codes with appropriate characters
-    """
-
-    word = re.sub('\\xe2\\x80\\x94', '-', word)
-    word = re.sub('\\xef\\xac\\x81', 'fi', word)
-    word = re.sub('\\xc2\\xb0', "DEGREE", word)
-    word = re.sub('\\xe2\\x80\\x99', "\'", word)
-    word = re.sub('\\xef\\xac\\x82', "fl", word)
-    word = re.sub('\\xc2\\xa3', 'POUND', word)
-    word = re.sub('\\xe2\\x80\\x98', "\'", word)
-    word = re.sub('\\xe2\\x80\\x9c', "\"", word)
-    word = re.sub('\\xe2\\x80\\x9d', "\"", word)
-    word = re.sub('\\xe2\\x80\\x93', "-", word)
-    word = re.sub('\\xe2\\x80\\x94', "--", word)
-    word = re.sub('\\xe2\\x80\\xa6', "...", word)
-    word = re.sub('\\xc2\\x82', ',', word)       # High code comma
-    word = re.sub('\\xc2\\x84', ',,', word)       # High code double comma
-    word = re.sub('\\xc2\\x85', '...', word)     # Tripple dot
-    word = re.sub('\\xc2\\x88', '^', word)       # High carat
-    word = re.sub('\\xc2\\x91', '\'', word)    # Forward single quote
-    word = re.sub('\\xc2\\x92', '\'', word)    # Reverse single quote
-    word = re.sub('\\xc2\\x93', '\"', word)    # Forward double quote
-    word = re.sub('\\xc2\\x94', '\"', word)    # Reverse double quote
-    word = re.sub('\\xc2\\x95', '_', word)
-    word = re.sub('\\xc2\\x96', '-', word)       # High hyphen
-    word = re.sub('\\xc2\\x97', '--', word)      # Double hyphen
-    word = re.sub('\\xc2\\x99', '_', word)
-    word = re.sub('\\xc2\\xa0', '_', word)
-    word = re.sub('\\xc2\\xa6', '|', word)       # Split vertical bar
-    word = re.sub('\\xc2\\xab', '<<', word)      # Double less than
-    word = re.sub('\\xc2\\xbb', '>>', word)      # Double greater than
-    word = re.sub('\\xc2\\xbc', '1/4', word)     # one quarter
-    word = re.sub('\\xc2\\xbd', '1/2', word)     # one half
-    word = re.sub('\\xc2\\xbe', '3/4', word)     # three quarters
-    word = re.sub('\\xca\\xbf', '\'', word)    # c-single quote
-    word = re.sub('\\xcc\\xa8', '', word)        # modifier - under curve
-    word = re.sub('\\xcc\\xb1', '', word)         # modifier - under line
-    word = re.sub('\\xc2\\xa7', 'CODE', word)
-
-    return word
 
 def extract(doc):
     """
