95af49e298085669899161047872c7525f2d863f
Tong Shu Li <tongli@scripps.edu>
2016-02-25 18:25:26 -0800

    Simplify false examples for distant supervision.

--- py/gene_relations.py
+++ py/gene_relations.py
@@ -650,32 +650,28 @@ def extract(doc):
                     output("\\N")
 
                 else:
-                    if w1.word in dict_no_interact and no_interact_words(ws):
+                    # only a false example if not in the gold standard
+                    if not_in_gold_std:
+                        if w1.word in dict_no_interact and no_interact_words(ws):
 
-                        if not_in_gold_std and w2.word in dict_no_interact[w1.word] and not high_quality_verb: 
+                            if w2.word in dict_no_interact[w1.word] and not high_quality_verb: 
+                                output("false")
+
+                        elif w2.word in dict_no_interact and no_interact_words(ws):
+
+                            if w1.word in dict_no_interact[w2.word] and not high_quality_verb:
+                                output("false")
+
+                        elif appear_in_same_doc(w1.word, w2.word, dict_pmid_gene) and no_interact_words(ws) and not high_quality_verb:
+                            # Negative Example: Mention appear in KB in same doc, but no interaction extracted in KB
+                            output("false")
+                        elif no_interact_phrase(ws) and not high_quality_verb:
                             output("false")
 
-                    elif w2.word in dict_no_interact and no_interact_words(ws):
-
-                        if not_in_gold_std and w1.word in dict_no_interact[w2.word] and not high_quality_verb:
+                        elif w1.ner == "Person" or w2.ner == "Person":
                             output("false")
 
-                    elif appear_in_same_doc(w1.word, w2.word, dict_pmid_gene) and no_interact_words(ws) and not high_quality_verb:
-                        # Negative Example: Mention appear in KB in same doc, but no interaction extracted in KB
-
-                        if not_in_gold_std:
-                            output("false")
-
-                    elif no_interact_phrase(ws) and not high_quality_verb:
-                        if not_in_gold_std:
-                            output("false")
-
-                    elif w1.ner == "Person" or w2.ner == "Person":
-                        if not_in_gold_std:
-                            output("false")
-
-                    elif random.random() < .08 and not high_quality_verb:
-                        if not_in_gold_std:
+                        elif random.random() < .08 and not high_quality_verb:
                             output("false")
 
                     output("\\N")
